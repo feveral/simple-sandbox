2c22d6e9aae4d95512124f3341fa44059bab14d2
feveral <feveraly@gmail.com>
2019-11-13 14:12:07 +0800

    update sandboxLib.h

--- include/sandboxLib.h
+++ include/sandboxLib.h
@@ -4,7 +4,12 @@
 #include <dirent.h>
 #include <sys/types.h>
 
+int getdevnull();
+int getstdout();
 void *getOriginFunction(const char *command);
+char *concat(const char *s1, const char *s2);
+char *cutPathTail(const char *path);
+int isContain(const char *str, char target);
 ssize_t readlink(const char *pathname, char *buf, size_t bufsiz);
 DIR *opendir(const char *name);
 int execv(const char *path, char *const argv[]);
